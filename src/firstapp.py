9613c7dd3dde9f14a90b58f174a6b2999c655e0d
Junior Nascimento <carlos.cstds@gmail.com>
2011-06-17 18:25:23 -0300

    mais um arquivo de aprendizado

---
+++ src/firstapp.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+
+#idade = raw_input("Qual a sua idade? ")
+#
+#isAllowed = True if int(idade) >= 18 else False
+#
+#if isAllowed:
+#    print "Bem-vindo ao site xotas net!"
+#else:
+#    print "Você será redirecionado ao site da disney!"
+
+
+#print "Esse número %d é inteiro" %(14)
+
+#name = raw_input("Qual seu nome? ")
+#print "Welcome, %s." %(name)
+
+
+#Changing base of numbers
+#print "Decimal: %d , Octal: %o, Hexa: %x" %(10,10,10)
+
+#method format()
+#musicians = [("René","Cantor","Calle 13"),("Zion","Cantor","Zion y Lennox")]
+#
+#msg = "{0} é {1} do {2}"
+#
+#for name, function, band in musicians:
+#    print(msg.format(name,function,band))
+
+
+#msg = '{saudacao}, são {hora:02d}:{minuto:02d} '
+#print msg.format(saudacao="Bom dia", hora=7,minuto=30)
+
